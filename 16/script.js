699e422ef37b7b900293a1529ce5512366ba7b81
Charles Desiderio <cjdesi@gmail.com>
2022-01-02 22:53:11 -0600

    Day 16 prototype working

--- 16/script.js
+++ 16/script.js
@@ -1,1 +1,18 @@
-console.log(document.getElementById('star'))
+let stars = document.querySelectorAll('.star')
+
+const callHover = (id) => {
+  for (let i = 0; i < id; i++) {
+    document.getElementById(i + 1).classList.add('highlighted')
+  }
+}
+
+const callRemoveHover = () => {
+  for (let i = 0; i < stars.length; i++) {
+    document.getElementById(i + 1).classList.remove('highlighted')
+  }
+}
+
+stars.forEach((item) => {
+  item.addEventListener('mouseover', () => callHover(item.id))
+  item.addEventListener('mouseout', callRemoveHover)
+})
